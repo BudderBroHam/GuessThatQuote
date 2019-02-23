0f21e87fa2b84e7cb50e23b09837dcab7913823d
BudderBroHam <37490500+BudderBroHam@users.noreply.github.com>
2019-02-22 23:52:29 -0500

    Update sketch.js

--- Games/Shooty Squares Mobile/sketch.js
+++ Games/Shooty Squares Mobile/sketch.js
@@ -13,35 +13,25 @@ var enemyalive = true
 
 function setup() {
   createCanvas(WIDTH,HEIGHT);
-  //sliders
-  XSlider = createSlider(0, 100, 50);
-  XSlider.position(50, 75);
-  YSlider = createSlider(0, 100, 50);
-  YSlider.position(150, 75)
-  YSlider.style('rotate', 90);
   //button
    button = createButton('Shoot');
-   button.position(250 , 50);
+   button.position(50 , 50);
    button.mousePressed(ButtonPressed);
    enemyy = random(100,600);
 }
 
 function draw() {
+ xposition = mouseX
+ yposition = mouseY
  fill(216, 206, 182)
  rect(0,0,WIDTH,HEIGHT)
  //making player
  fill(255,0,0)
  rect(xposition,yposition,50,50)
- //finding what position to go from the speed
- xposition += (xspeed - 50)/10
- yposition += (yspeed - 50)/10
- //getting slider values
- xspeed = XSlider.value();
- yspeed = YSlider.value();
- //idk anymore
+ //shoot loop
  if (shoot == true) {
   fill(0,0,255)
-  rect(xposition+bulletx + 50,bullety,10,10)
+  rect(bulletx + 50,bullety,10,10)
   bulletx +=10
  }
  //enemy
@@ -52,13 +42,12 @@ function draw() {
  }
  if (xposition+bulletx + 50 > enemyx && xposition+bulletx + 50 < enemyx + 50 && bullety > enemyy && bullety < enemyy + 50)  {
   enemyalive = false
-  print("nice")
  }
  
 }
 
 function ButtonPressed() {
-  bulletx = 0
+  bulletx = xposition
   bullety = yposition + 25
   shoot = true
 }
