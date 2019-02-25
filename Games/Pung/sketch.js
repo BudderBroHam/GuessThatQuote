43e2adb4ab7e40fe98ae8181dd46c14708726ffe
BudderBroHam <37490500+BudderBroHam@users.noreply.github.com>
2019-02-24 21:03:27 -0500

    Update sketch.js

--- Games/Pung/sketch.js
+++ Games/Pung/sketch.js
@@ -78,7 +78,7 @@ function draw() {
   }
   //if max difficulty go SICKO MODE
   if (difficulty >= 9) {
-     enemyyposition = bally
+     enemyyposition = bally + 75
   }
   //do the do
   difficulty = difficultyslider.value();
